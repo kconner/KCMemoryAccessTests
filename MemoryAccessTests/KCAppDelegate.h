8296ba8e477698a3fc6a6a1de133d11d658866c1
Kevin Conner <connerk@gmail.com>
2012-12-02 18:55:09 -0500

    Initial Commit

---
+++ MemoryAccessTests/KCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KCAppDelegate.h
+//  MemoryAccessTests
+//
+//  Created by Kevin Conner on 12/2/12.
+//  Copyright (c) 2012 Kevin Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
